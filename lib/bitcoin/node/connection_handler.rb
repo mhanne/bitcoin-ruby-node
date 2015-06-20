d9a532a8b13aad58adcb212e0a9ea6570fa21fc4
Marius Hanne <marius.hanne@sourceagency.org>
2015-06-20 17:52:34 +0200

    handle parse errors gracefully

--- lib/bitcoin/node/connection_handler.rb
+++ lib/bitcoin/node/connection_handler.rb
@@ -111,6 +111,11 @@ module Bitcoin::Node
       send_data P::Addr.pkt(@node.addr)  if @node.config[:announce]
     end
 
+    # error parsing a message, log as warning but otherwise ignore
+    def on_error(type, data)
+      log.warn { "error: #{type} (#{data})" }
+    end
+
     # received +inv_tx+ message for given +hash+.
     # add to inv_queue, unlesss maximum is reached
     def on_inv_transaction(hash)
